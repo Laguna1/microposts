4f8500b51772ece6cd4104c3c22b0d6577d55839
Laguna1 <lagunazoomarket@gmail.com>
2022-01-08 00:51:17 +0200

    Add Contact page, fix routes

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,19 +4,25 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test 'should get home' do
-    get :home
+    get 'static_pages/home'
     assert_response :success
     assert_select 'title', 'Microposts'
   end
 
   test 'should get help' do
-    get :help
+    get 'static_pages_help_path'
     assert_response :success
     assert_select 'title', 'Microposts'
   end
 
   test 'should get about' do
-    get :about
+    get 'static_pages/about'
+    assert_response :success
+    assert_select 'title', 'Microposts'
+  end
+
+  test 'should get contact' do
+    get 'static_pages/contact'
     assert_response :success
     assert_select 'title', 'Microposts'
   end
