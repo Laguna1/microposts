15d901871775d79e89d95925060b6e26fdf8ab61
Laguna1 <lagunazoomarket@gmail.com>
2022-01-07 19:48:34 +0200

    Add full_title method

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -6,18 +6,18 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test 'should get home' do
     get :home
     assert_response :success
-    assert_select 'title', 'Home | Microposts'
+    assert_select 'title', 'Microposts'
   end
 
   test 'should get help' do
     get :help
     assert_response :success
-    assert_select 'title', 'Help | Microposts'
+    assert_select 'title', 'Microposts'
   end
 
   test 'should get about' do
     get :about
     assert_response :success
-    assert_select 'title', 'About | Microposts'
+    assert_select 'title', 'Microposts'
   end
 end
