731978c6420a69474c2eb71fb436b24e7d89662e
Laguna1 <lagunazoomarket@gmail.com>
2022-01-08 01:41:03 +0200

    Add full_title helper test

---
+++ test/helpers/application_helper_test.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class ApplicationHelperTest < ActionView::TestCase
+  test 'full title helper' do
+    assert_equal full_title, base_title = 'Microposts'
+    assert_equal full_title('Help'), "Help | #{base_title}"
+  end
+end
