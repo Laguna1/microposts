348bb3cd63799870c054187e4df5247d57e0760a
Laguna1 <lagunazoomarket@gmail.com>
2022-01-08 01:13:01 +0200

    Fix static_pages_controller_test, add integration site_layout test

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,26 +4,26 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test 'should get home' do
-    get 'static_pages/home'
+    get '/'
     assert_response :success
-    assert_select 'title', 'Microposts'
+    assert_select 'title', 'Home | Microposts'
   end
 
   test 'should get help' do
-    get 'static_pages_help_path'
+    get '/static_pages/help'
     assert_response :success
-    assert_select 'title', 'Microposts'
+    assert_select 'title', 'Help | Microposts'
   end
 
   test 'should get about' do
-    get 'static_pages/about'
+    get '/static_pages/about'
     assert_response :success
-    assert_select 'title', 'Microposts'
+    assert_select 'title', 'About | Microposts'
   end
 
   test 'should get contact' do
-    get 'static_pages/contact'
+    get '/static_pages/contact'
     assert_response :success
-    assert_select 'title', 'Microposts'
+    assert_select 'title', 'Contact | Microposts'
   end
 end
