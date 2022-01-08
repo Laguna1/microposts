eac0f63b9f20615532983f2b089e8932d847d81d
Laguna1 <lagunazoomarket@gmail.com>
2022-01-08 02:11:24 +0200

    Add index to email in user table to provide the email uniqueness

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  before_save { self.email = email.downcase }
+
   has_many :microposts
   validates :name, presence: true, uniqueness: true, length: { minimum: 3, maximum: 25 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
