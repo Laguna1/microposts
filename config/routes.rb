9f1543a91cbee940921d575429bde3596dd00dba
Laguna1 <lagunazoomarket@gmail.com>
2022-01-08 22:34:58 +0200

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'sessions/new'
-  root 'static_pages#home'
-  get 'static_pages/help', as: :help
-  get 'static_pages/about', as: :about
-  get 'static_pages/contact', as: :contact
-  resources :microposts
+  root   'static_pages#home'
+  get    '/help',    to: 'static_pages#help'
+  get    '/about',   to: 'static_pages#about'
+  get    '/contact', to: 'static_pages#contact'
+  get    '/signup',  to: 'users#new'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
   resources :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-end
+  resources :microposts
+ end
