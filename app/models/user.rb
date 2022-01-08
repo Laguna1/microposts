50479bea6796f3740da834d808b1f5b3d99f5587
Laguna1 <lagunazoomarket@gmail.com>
2022-01-08 02:05:10 +0200

    Add User model tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,10 @@
 
 class User < ApplicationRecord
   has_many :microposts
-  validates :name, presence: true, uniqueness: true
-  validates :email, presence: true, uniqueness: true
+  validates :name, presence: true, uniqueness: true, length: { minimum: 3, maximum: 25 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
+  validates :email, presence: true,
+                    uniqueness: { case_sensitive: false },
+                    length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX }
 end
