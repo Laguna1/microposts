bfb61ae4841c68cff53d42b55f480ba837cd21e7
Laguna1 <lagunazoomarket@gmail.com>
2022-01-08 19:51:31 +0200

    Add successfull user registration taes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,14 +26,12 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
 
-  
-      if @user.save
-        flash[:success] = 'Welcome to the Microposts Blog!'
-        redirect_to user_url(@user)
-      else
-        render :new
-      end
-    
+    if @user.save
+      flash[:success] = 'Welcome to the Microposts Blog!'
+      redirect_to user_url(@user)
+    else
+      render :new
+    end
   end
 
   # PATCH/PUT /users/1 or /users/1.json
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -13,4 +13,16 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     end
     assert_template 'users/new'
   end
+
+  test 'valid signup information' do
+    get new_user_path
+    assert_difference 'User.count', 1 do
+      post users_path, params: { user: { name: 'Example User',
+                                         email: 'user@example.com',
+                                         password: 'password',
+                                         password_confirmation: 'password' } }
+      follow_redirect!
+    end
+    assert_template 'users/show'
+  end
 end
