758d2a6a07c28ca145dbb28d0c4e6be3d4c1e0dd
Laguna1 <lagunazoomarket@gmail.com>
2022-01-08 02:44:30 +0200

    Add user model test

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  before_save { self.email = email.downcase }
+  before_save { email.downcase! }
 
   has_many :microposts
   validates :name, presence: true, uniqueness: true, length: { minimum: 3, maximum: 25 }
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true,
                     uniqueness: { case_sensitive: false },
                     length: { maximum: 255 },
